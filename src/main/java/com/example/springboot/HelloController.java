b6d65c53af171a0140515574e4567907d12c0b75
Dimitrios Prapas <dimitrisprapas@gmail.com>
2021-10-30 14:23:22 +0300

    Logging added

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,8 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
+		Logger logger = LoggerFactory.getLogger(HelloController.class);
+		logger.info("home controller started...");
 		return "Greetings from Spring Boot with Github Actions with removed azure maven plugin!";
 	}
 
